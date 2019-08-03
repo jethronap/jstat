91ef3dd940b748c123b79a7585b89730ddb40873
jethronap <efthioa@gmail.com>
2019-08-03 18:04:06 +0300

    created parseJson method

---
+++ src/main/java/dataLoader/jsonDataLoader.java
@@ -0,0 +1,40 @@
+package dataLoader;
+
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Simple Class that parses data set from a json file
+ * without knowing the files format
+ * & prints out the key value pairs.
+ */
+
+
+
+public class jsonDataLoader {
+
+    private String location = "where the file is located";
+    private File json = new File(location);
+
+    public void parseJsonData(String json) throws IOException {
+
+
+        JsonFactory factory = new JsonFactory();
+
+        ObjectMapper mapper = new ObjectMapper(factory);
+        JsonNode rootNode = mapper.readTree(json);
+
+        Iterator<Map.Entry<String, JsonNode>> fieldsIterator = rootNode.fields();
+        while (fieldsIterator.hasNext()) {
+            Map.Entry<String, JsonNode> field = fieldsIterator.next();
+            System.out.println("Key: " + field.getKey() + "\tValue: " + field.getValue());
+        }
+    }
+}
