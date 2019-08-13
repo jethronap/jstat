c3039adf3fde2b742d8ec8ac6c90ec369536a61f
pockerman <upperframeworks@gmail.com>
2019-08-13 09:22:20 +0300

    Add generic parameter in ISample

--- src/main/java/datastructs/ISample.java
+++ src/main/java/datastructs/ISample.java
@@ -4,7 +4,7 @@ package datastructs;
 /**
  * Interface to model a sample from the data
  */
-public interface ISample {
+public interface ISample<T> {
 
     /**
      * The name of the sample
@@ -15,5 +15,15 @@ public interface ISample {
      * Returns the size of the sample
      */
     public int getsize();
+	
+	/**
+	  * Returns a SampleView of the sample
+	  */
+	public SampleView<T> getView(int start, int end);
+	
+	/**
+	  * Prints information about the sample
+	  */
+	public void printInfo();  
 
 }
