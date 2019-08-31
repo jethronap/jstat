e34b9f3c8ad710c27ed8780e095bbba0b415519f
jethronap <efthioa@gmail.com>
2019-08-31 18:33:37 +0300

    created method to parse csv dataset with header.

--- src/main/java/dataloader/CsvDataLoader.java
+++ src/main/java/dataloader/CsvDataLoader.java
@@ -4,19 +4,20 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import tech.tablesaw.api.Table;
+import tech.tablesaw.io.csv.CsvReadOptions;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.TreeMap;
 
-/**
- * Simple class that parses data set from a csv file
- * without knowing the headers of the file
- */
 
 public class CsvDataLoader {
 
+    /**
+     * Simple class that parses data set from a csv file
+     * without knowing the headers of the file
+     */
     public void parseFile(String csvFile, TreeMap dataSet) throws IOException {
 
         Reader csvData = new FileReader(csvFile);
@@ -33,9 +34,14 @@ public class CsvDataLoader {
         }
     }
 
-    public void parseFile(Table csvData) throws IOException {
+    /**
+     * Reads from csv in file system with columns separated by commas
+     * and the file has a header row.
+     * Missing values are treated with an indicator.
+     * */
+    public void parseFile(String csvFile, Table csvData) throws IOException {
         //Table csvData = Table.read().csv("csvFile.csv");
-        csvData.read().file("myFile.csv");
+        csvData.read().usingOptions(CsvReadOptions.builder(csvFile).missingValueIndicator("-"));
     }
 
 }
