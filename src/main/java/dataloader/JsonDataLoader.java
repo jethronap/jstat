3589734d3df26d78e69f9d65b942cb72273c6443
jethronap <efthioa@gmail.com>
2019-08-31 20:13:24 +0300

    formatted code

--- src/main/java/dataloader/JsonDataLoader.java
+++ src/main/java/dataloader/JsonDataLoader.java
@@ -8,7 +8,6 @@ import tech.tablesaw.api.Table;
 import tech.tablesaw.io.json.JsonReadOptions;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -25,29 +24,27 @@ public class JsonDataLoader {
     public void parseFile(String jsonFile, HashMap<String, Object> dataSet) throws IOException {
 
         File json = new File(jsonFile);
-        try {
-            JsonFactory factory = new JsonFactory();
-            ObjectMapper mapper = new ObjectMapper(factory);
-            JsonNode rootNode = mapper.readTree(json);
 
-            Iterator<Map.Entry<String, JsonNode>> fieldsIterator = rootNode.fields();
-            while (fieldsIterator.hasNext()) {
-                Map.Entry<String, JsonNode> field = fieldsIterator.next();
-                dataSet.put(field.getKey(), field.getValue());
+        JsonFactory factory = new JsonFactory();
+        ObjectMapper mapper = new ObjectMapper(factory);
+        JsonNode rootNode = mapper.readTree(json);
 
-                System.out.println(dataSet);
-                System.out.println("Key: " + field.getKey() + "\tValue: " + field.getValue());
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println(e);
+        Iterator<Map.Entry<String, JsonNode>> fieldsIterator = rootNode.fields();
+        while (fieldsIterator.hasNext()) {
+            Map.Entry<String, JsonNode> field = fieldsIterator.next();
+            dataSet.put(field.getKey(), field.getValue());
+
+            System.out.println(dataSet);
+            System.out.println("Key: " + field.getKey() + "\tValue: " + field.getValue());
         }
 
     }
+
     /**
      * Method that accepts json format as arrays,
      * with or without headers and
      * array of objects.
-     * */
+     */
     public void parseFile(String jsonFile, Table dataSet) throws IOException {
         //jsonData.read().file("myFile.json");
         dataSet.read().usingOptions(JsonReadOptions.builder(jsonFile));
