8bff910501561f75055854ce6114fe54a82cf721
jethronap <efthioa@gmail.com>
2019-09-02 09:58:10 +0300

    removed Table instantiation from test

--- src/test/java/dataloader/CsvLoaderTest.java
+++ src/test/java/dataloader/CsvLoaderTest.java
@@ -1,7 +1,6 @@
 package dataloader;
 
 import org.junit.Test;
-import tech.tablesaw.api.Table;
 
 import java.io.IOException;
 import java.util.TreeMap;
@@ -27,7 +26,7 @@ public class CsvLoaderTest {
     @Test(expected = IOException.class)
     public void testCsvFileEmptyStringForTable() throws IOException {
         CsvDataLoader loader = new CsvDataLoader();
-        Table dataSet = null;
-        loader.parseFile("nonExistent.csv", dataSet);
+        //Table dataSet = new Table();
+        loader.parseFile("nonExistent.csv");
     }
 }
