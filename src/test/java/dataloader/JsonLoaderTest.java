4b9d7fbfc262d48af70641d7292174b6522f0164
jethronap <efthioa@gmail.com>
2019-09-03 22:31:05 +0300

    added unit tests

--- src/test/java/dataloader/JsonLoaderTest.java
+++ src/test/java/dataloader/JsonLoaderTest.java
@@ -3,10 +3,15 @@ package dataloader;
 import org.junit.Test;
 import tech.tablesaw.api.Table;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
 public class JsonLoaderTest {
+
     /**
      * Test Scenario: The application wants to read a json dataset
      * but the String is invalid(empty String).
@@ -18,15 +23,34 @@ public class JsonLoaderTest {
         JsonDataLoader loader = new JsonDataLoader();
         loader.parseFile("nonExistent.json", new HashMap<String, Object>());
     }
+
     /**
      * This is the same test as above
      * but for type Table.
-     * */
+     */
 
     @Test(expected = IOException.class)
     public void testEmptyStringForTable() throws IOException {
         JsonDataLoader loader = new JsonDataLoader();
         Table dataSet = null;
         loader.parseFile("nonExistent.json", dataSet);
+    }
+
+    /**
+     * Test Scenario: a json file is provided
+     * Expected Output: the dataset is returned
+     * along with the correct column size and row count.
+     */
+
+    @Test
+    public void testJsonFileForTable() throws IOException {
+        JsonDataLoader loader = new JsonDataLoader();
+        Table dataSet = null;
+        File file = new File("test_data/dummy.json");
+        dataSet = loader.parseFile(file, dataSet);
+
+        assertNotNull(dataSet);
+        assertEquals(dataSet.columns().size(), 3);
+        assertEquals(dataSet.rowCount(), 1);
     }
 }
