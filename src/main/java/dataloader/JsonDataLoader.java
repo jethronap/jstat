914098ec45db99ff630002657995eba996d7361e
jethronap <efthioa@gmail.com>
2019-09-04 19:57:45 +0300

    added MapLoader class & converted dataSet into local variable

--- src/main/java/dataloader/JsonDataLoader.java
+++ src/main/java/dataloader/JsonDataLoader.java
@@ -8,7 +8,9 @@ import tech.tablesaw.api.Table;
 import tech.tablesaw.io.json.JsonReadOptions;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -22,33 +24,41 @@ import java.util.Map;
 
 public class JsonDataLoader {
 
-    /**
-     * Method that parses data set from a json file
-     * without knowing the file's format
-     * & gets the key value pairs.
-     * Doesn't accept json in array format.
-     */
-    public HashMap parseFile(String jsonFile, HashMap<String, Object> dataSet) throws IOException {
+    public static class MapLoader {
+        /**
+         * Method that parses data set from a json file
+         * without knowing the file's format
+         * & gets the key value pairs.
+         * Doesn't accept json in array format.
+         */
+        public static HashMap parseFile(File jsonFile) throws IOException {
 
-        File json = new File(jsonFile);
+            Reader json = new FileReader(jsonFile);
+            HashMap<String, Object> dataSet = new HashMap<>();
 
-        JsonFactory factory = new JsonFactory();
-        ObjectMapper mapper = new ObjectMapper(factory);
-        JsonNode rootNode = mapper.readTree(json);
+            JsonFactory factory = new JsonFactory();
+            ObjectMapper mapper = new ObjectMapper(factory);
+            JsonNode rootNode = mapper.readTree(json);
 
-        Iterator<Map.Entry<String, JsonNode>> fieldsIterator = rootNode.fields();
-        while (fieldsIterator.hasNext()) {
-            Map.Entry<String, JsonNode> field = fieldsIterator.next();
-            dataSet.put(field.getKey(), field.getValue());
+            Iterator<Map.Entry<String, JsonNode>> fieldsIterator = rootNode.fields();
+            while (fieldsIterator.hasNext()) {
+                Map.Entry<String, JsonNode> field = fieldsIterator.next();
+                dataSet.put(field.getKey(), field.getValue());
+            }
+            return dataSet;
         }
-        return dataSet;
+
+        public static HashMap parseFile(String jsonFile) throws IOException {
+            File file = new File(jsonFile);
+            return MapLoader.parseFile(file);
+        }
     }
 
     /**
      * Reads from a json file.
      * Accepts json format as arrays,
      * with or without headers, array of objects.
-     * A Table dataSet is returned.
+     * A dataSet in tablesaw format is returned.
      */
     public Table parseFile(String jsonFile, Table dataSet) throws IOException {
         JsonReadOptions options = JsonReadOptions.builder(jsonFile).build();
@@ -61,7 +71,7 @@ public class JsonDataLoader {
      * Reads from json a file.
      * Accepts json format as arrays,
      * with or without headers, array of objects.
-     * A Table dataSet is returned.
+     * A dataSet in tablesaw format is returned.
      */
     public Table parseFile(File jsonFile, Table dataSet) throws IOException {
         JsonReadOptions options = JsonReadOptions.builder(jsonFile).build();
