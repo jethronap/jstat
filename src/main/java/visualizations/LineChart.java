c6efd7b2aa97d7757f1dcc7d85305757faceefd4
jethronap <efthioa@gmail.com>
2019-09-11 00:59:59 +0300

    created method to plot smooth lines

--- src/main/java/visualizations/LineChart.java
+++ src/main/java/visualizations/LineChart.java
@@ -9,15 +9,15 @@ import tech.tablesaw.plotly.traces.ScatterTrace;
 
 /**
  * Class that plots basic Line Charts.
- * */
+ */
 
 public class LineChart {
 
     /**
-     *  Plots a line given the chart title,
-     *  and two numeric columns.
-     * */
-    public static void plotLine (
+     * Plots a line given the chart title,
+     * and two numeric columns.
+     */
+    public static void plotLine(
             String chartTitle, NumericColumn x, NumericColumn y) {
 
         Layout layout = Layout.builder().title(chartTitle).build();
@@ -26,5 +26,21 @@ public class LineChart {
 
     }
 
+    /**
+     * Plots a line given the chart title,
+     * two numeric columns and custom
+     * smoothing.
+     */
+
+    public static void plotSmoothLine(
+            String chartTitle, NumericColumn x, NumericColumn y, Double smoothing) {
+
+        Layout layout = Layout.builder().title(chartTitle).build();
+        ScatterTrace trace = ScatterTrace.builder(x, y)
+                .mode(ScatterTrace.Mode.LINE)
+                .line(Line.builder().shape(Line.Shape.SPLINE).smoothing(smoothing).build())
+                .build();
+        Plot.show(new Figure(layout, trace));
+    }
 
 }
