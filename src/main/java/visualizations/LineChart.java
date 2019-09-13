515829b8aa666a8653c37264311fa3e0b903ce34
jethronap <efthioa@gmail.com>
2019-09-13 21:07:47 +0300

    corrected smooth method to accept Table data set

--- src/main/java/visualizations/LineChart.java
+++ src/main/java/visualizations/LineChart.java
@@ -1,6 +1,5 @@
 package visualizations;
 
-import tech.tablesaw.api.NumericColumn;
 import tech.tablesaw.api.Table;
 import tech.tablesaw.plotly.Plot;
 import tech.tablesaw.plotly.api.LinePlot;
@@ -33,10 +32,10 @@ public class LineChart {
      */
 
     public static void plotSmoothLine(
-            String chartTitle, NumericColumn x, NumericColumn y, Double smoothing) {
+            String chartTitle, Table table, int x, int y, Double smoothing) {
 
         Layout layout = Layout.builder().title(chartTitle).build();
-        ScatterTrace trace = ScatterTrace.builder(x, y)
+        ScatterTrace trace = ScatterTrace.builder(table.numberColumn(x), table.numberColumn(y))
                 .mode(ScatterTrace.Mode.LINE)
                 .line(Line.builder().shape(Line.Shape.SPLINE).smoothing(smoothing).build())
                 .build();
