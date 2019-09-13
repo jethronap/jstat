aa19807d3dd8b917d2361f501c1bc61972e49f31
jethronap <efthioa@gmail.com>
2019-09-14 02:19:22 +0300

    added unit tests for pie chart

---
+++ src/test/java/visualizations/PieChartTest.java
@@ -0,0 +1,29 @@
+package visualizations;
+
+import dataloader.CsvDataLoader;
+import org.junit.Test;
+import tech.tablesaw.api.Table;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PieChartTest {
+
+
+    /**
+     *  Test Scenario: The user provides a data set
+     *  and wants to plot a pie chart.
+     *  Expected Output: The chart is plotted correctly.
+     * */
+    @Test
+    public void testPieShow() throws IOException {
+
+        File file = new File("test_data/humans_data.csv");
+        Table table = CsvDataLoader.TableLoader.parseFile(file);
+
+        PieChart chart = new PieChart();
+        chart.plotPie(
+                "hair color by age", table,
+                "HairColor", "Age");
+    }
+}
