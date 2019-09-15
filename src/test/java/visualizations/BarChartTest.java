d6d26284a404b555426c5346fe715ba5944f5132
jethronap <efthioa@gmail.com>
2019-09-15 21:47:51 +0300

    created unit test for horizontal bar chart

--- src/test/java/visualizations/BarChartTest.java
+++ src/test/java/visualizations/BarChartTest.java
@@ -25,4 +25,20 @@ public class BarChartTest {
                 "height by sex", table,
                 "Sex", "Height");
     }
+
+    /**
+     * Test Scenario: The user provides a data set
+     * and wants to plot a horizontal bar chart.
+     * Expected Output: The chart is plotted correctly.
+     */
+    @Test
+    public void testPlotHorizontalBar() throws IOException {
+        File file = new File("test_data/humans_data.csv");
+        Table table = CsvDataLoader.TableLoader.parseFile(file);
+
+        BarChart chart = new BarChart();
+        chart.plotHorizontalBar(
+                "height by sex", table,
+                "Sex", "Height");
+    }
 }
