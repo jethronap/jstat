aa588fdeab6d9006b590f2a1f2f9435268122670
jethronap <efthioa@gmail.com>
2019-09-17 23:07:11 +0300

    refactored options and added method for numeric sample

--- src/main/java/visualizations/LineChart.java
+++ src/main/java/visualizations/LineChart.java
@@ -1,5 +1,7 @@
 package visualizations;
 
+import datastructs.NumericSample;
+import tech.tablesaw.api.DoubleColumn;
 import tech.tablesaw.api.Table;
 import tech.tablesaw.plotly.Plot;
 import tech.tablesaw.plotly.api.LinePlot;
@@ -14,12 +16,27 @@ import tech.tablesaw.plotly.traces.ScatterTrace;
 
 public class LineChart {
 
-    static class LineChartOptions {
-        String chartTitle;
-        String nameXCol;
-        String nameYCol;
-        Double smoothing;
+    public static class LineChartOptions {
+        public String chartTitle;
+        public double smoothing;
+        public String xAxisName;
+        public String yAxisName;
 
+    }
+
+    /**
+     * Plots a line given the chart title,
+     * data set in Table format,
+     * and two numeric columns
+     * created from a numeric sample
+     * of the data set.
+     */
+    public static void plotLine(NumericSample x, NumericSample y, LineChartOptions options){
+
+        DoubleColumn xcol = DoubleColumn.create(options.xAxisName, x.asArray());
+        DoubleColumn ycol = DoubleColumn.create(options.yAxisName, y.asArray());
+        Table table = Table.create(xcol, ycol);
+        Plot.show(LinePlot.create(options.chartTitle, table, options.xAxisName, options.yAxisName));
     }
 
     /**
@@ -29,7 +46,7 @@ public class LineChart {
      */
     public static void plotLine(LineChartOptions options, Table data) {
 
-        Plot.show(LinePlot.create(options.chartTitle, data, options.nameXCol, options.nameYCol));
+        Plot.show(LinePlot.create(options.chartTitle, data, options.xAxisName, options.yAxisName));
 
     }
 
@@ -44,7 +61,7 @@ public class LineChart {
 
         Layout layout = Layout.builder().title(options.chartTitle).build();
         ScatterTrace trace = ScatterTrace
-                .builder(data.numberColumn(options.nameXCol), data.numberColumn(options.nameYCol))
+                .builder(data.numberColumn(options.xAxisName), data.numberColumn(options.yAxisName))
                 .mode(ScatterTrace.Mode.LINE)
                 .line(Line.builder().shape(Line.Shape.SPLINE).smoothing(options.smoothing).build())
                 .build();
