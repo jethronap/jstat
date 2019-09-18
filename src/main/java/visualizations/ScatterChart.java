e42fbd81a8a54ec1182e03c9b124a0edb24c4a73
jethronap <efthioa@gmail.com>
2019-09-18 19:19:44 +0300

    created method for 3D scatter plotting

--- src/main/java/visualizations/ScatterChart.java
+++ src/main/java/visualizations/ScatterChart.java
@@ -3,6 +3,7 @@ package visualizations;
 
 import tech.tablesaw.api.Table;
 import tech.tablesaw.plotly.Plot;
+import tech.tablesaw.plotly.api.BubblePlot;
 import tech.tablesaw.plotly.api.ScatterPlot;
 
 /**
@@ -11,11 +12,12 @@ import tech.tablesaw.plotly.api.ScatterPlot;
 
 public class ScatterChart {
 
-    public class ScatterChartOption {
+    public class ScatterChartOptions {
         public String chartTitle;
         public String xAxisName;
         public String yAxisName;
         public String groupColName;
+        public String sizeColName;
     }
 
     /**
@@ -24,7 +26,7 @@ public class ScatterChart {
      * and two numeric variables.
      */
 
-    public static void plotScatter(ScatterChartOption options, Table data) {
+    public static void plotScatter(ScatterChartOptions options, Table data) {
 
         Plot.show(ScatterPlot.create(options.chartTitle, data, options.xAxisName, options.yAxisName));
     }
@@ -36,11 +38,22 @@ public class ScatterChart {
      * one categorical.
      */
 
-    public static void plotScatterWithCategorical(ScatterChartOption options, Table data) {
+    public static void plotScatterWithCategorical(ScatterChartOptions options, Table data) {
 
         Plot.show(
                 ScatterPlot.create(options.chartTitle,
                         data, options.xAxisName, options.yAxisName, options.groupColName));
     }
 
+    /**
+     * Plots a bubble chart given the chart title,
+     * data set in Table format,
+     * and three numeric variables.
+     */
+
+    public static void plotScatter3D (ScatterChartOptions options, Table data) {
+
+        Plot.show(BubblePlot.create(options.chartTitle,
+                data, options.xAxisName, options.yAxisName, options.sizeColName));
+    }
 }
