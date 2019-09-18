1ff5ecff963e45f1b827d8c21df05d98c5e61154
jethronap <efthioa@gmail.com>
2019-09-18 20:16:06 +0300

    added example

---
+++ src/main/java/examples/PlotAScatter.java
@@ -0,0 +1,30 @@
+package examples;
+
+import dataloader.CsvDataLoader;
+import tech.tablesaw.api.Table;
+import visualizations.ScatterChart;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Category: Plotting
+ * ID: PlotABar
+ * Description: Load a CSV file and plot a scatter chart with 3 numeric variables
+ */
+public class PlotAScatter {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("test_data/humans_data.csv");
+        Table data = CsvDataLoader.TableLoader.parseFile(file);
+
+        ScatterChart plotter = new ScatterChart();
+        ScatterChart.ScatterChartOptions options = plotter.new ScatterChartOptions();
+        options.chartTitle = "weight by age and height";
+        options.xAxisName = "Age";
+        options.yAxisName = "Height";
+        options.sizeColName = "Weight";
+
+        ScatterChart.plotScatter3D(options, data);
+    }
+}
