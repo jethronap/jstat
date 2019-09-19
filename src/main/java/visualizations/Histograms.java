2694e5f3a43b8ffa1efa3af0d1f1aad603c49d44
jethronap <efthioa@gmail.com>
2019-09-19 20:00:05 +0300

    added method for 2D plotting

--- src/main/java/visualizations/Histograms.java
+++ src/main/java/visualizations/Histograms.java
@@ -3,6 +3,7 @@ package visualizations;
 import tech.tablesaw.api.Table;
 import tech.tablesaw.plotly.Plot;
 import tech.tablesaw.plotly.api.Histogram;
+import tech.tablesaw.plotly.api.Histogram2D;
 
 /**
  * Class that plots basic Histograms.
@@ -12,7 +13,8 @@ public class Histograms {
 
     public class HistogramOptions {
         public String chartTitle;
-        public String columnName;
+        public String xAxisName;
+        public String yAxisName;
     }
 
     /**
@@ -23,6 +25,16 @@ public class Histograms {
 
     public static void plotHistogram(HistogramOptions options, Table data) {
 
-        Plot.show(Histogram.create(options.chartTitle, data, options.columnName));
+        Plot.show(Histogram.create(options.chartTitle, data, options.xAxisName));
+    }
+
+    /**
+     * Plots a histogram given the chart title,
+     * data set in Table format,
+     * and two column names.
+     */
+
+    public static void plotHistogram2D(HistogramOptions options, Table data) {
+        Plot.show(Histogram2D.create(options.chartTitle, data, options.xAxisName, options.yAxisName));
     }
 }
