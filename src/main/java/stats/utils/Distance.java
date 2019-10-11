332acc68785cf5c95063275a36ad9b0badaa833b
jethronap <efthioa@gmail.com>
2019-10-11 14:22:53 +0300

    added documentation for Distance interface

--- src/main/java/stats/utils/Distance.java
+++ src/main/java/stats/utils/Distance.java
@@ -3,9 +3,16 @@ package stats.utils;
 import java.util.Map;
 
 /**
- * Defines a contract to calculate distance between two vectors.
+ * Defines a contract to calculate distance between two sets of properties.
  */
 public interface Distance {
 
+    /**
+     *
+     * @param v1 The first set.
+     * @param v2 The second set.
+     * @return The calculated distance.
+     * @throws IllegalArgumentException If the given set is invalid.
+     */
     double calculate(Map<String, Double> v1, Map<String, Double> v2);
 }
