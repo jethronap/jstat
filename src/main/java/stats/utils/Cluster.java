319b66e285060b49c88e08f59ba7cce85c9308aa
jethronap <efthioa@gmail.com>
2019-11-07 17:22:44 +0200

    #43 created Cluster class

---
+++ src/main/java/stats/utils/Cluster.java
@@ -0,0 +1,47 @@
+package stats.utils;
+
+import java.util.List;
+
+public class Cluster {
+
+    public List getPoints() {
+        return points;
+    }
+
+    public void setPoints(List points) {
+        this.points = points;
+    }
+
+    public Point getCentroid() {
+        return centroid;
+    }
+
+    public void setCentroid(Point centroid) {
+        this.centroid = centroid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * List of Points in Cluster.
+     */
+    private List points;
+
+
+    /**
+     * The central Point.
+     */
+    private Point centroid;
+
+
+    /**
+     * Cluster's id.
+     */
+    private int id;
+}
