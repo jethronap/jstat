a0f9791d46aca1c2ddfadfa237978f36dc2e8ff1
Alex Giavaras <a.giavaras@gmail.com>
2020-02-04 10:18:29 +0200

    #56 Add new examples

---
+++ src/main/java/examples/ml/example3/Example3.java
@@ -0,0 +1,73 @@
+package examples.ml.example3;
+
+import maths.DenseMatrixSet;
+import maths.RowBuilder;
+import maths.Vector;
+import maths.RowType;
+import maths.functions.distances.EuclideanVectorCalculator;
+import ml.classifiers.KNNClassifier;
+import ml.classifiers.utils.ClassificationVoter;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** Category: Machine Learning
+ * ID: Example1
+ * Description: Classification with vanilla KNN algorithm
+ * Taken From:
+ * Details:
+ * TODO
+ */
+
+
+public class Example3 {
+
+    public static void main(String[] args) {
+
+        DenseMatrixSet<Double> dataSet = new DenseMatrixSet(RowType.Type.DOUBLE_VECTOR, new RowBuilder());
+        dataSet.create(12, 2);
+        dataSet.set(0, 1.0, 3.0);
+        dataSet.set(1, 1.5, 2.0);
+        dataSet.set(2, 2.0, 1.0);
+        dataSet.set(3, 2.5, 4.0);
+        dataSet.set(4, 3.0, 1.5);
+        dataSet.set(5, 3.5, 2.5);
+        dataSet.set(6, 5.0, 5.0);
+        dataSet.set(7, 5.5, 4.0);
+        dataSet.set(8, 6.0, 6.0);
+        dataSet.set(9, 6.5, 4.5);
+        dataSet.set(10, 7.0, 1.5);
+        dataSet.set(11, 8.0, 2.5);
+
+        List<Integer> labels = new ArrayList<>(dataSet.m());
+
+        for (int i = 0; i < 6; ++i) {
+            labels.add(0);
+        }
+
+        for (int i = 6; i < dataSet.m(); ++i) {
+            labels.add(1);
+        }
+
+
+        KNNClassifier<Double, DenseMatrixSet<Double>,
+                EuclideanVectorCalculator<Double>,
+                ClassificationVoter> classifier = new KNNClassifier<Double, DenseMatrixSet<Double>,
+                EuclideanVectorCalculator<Double>, ClassificationVoter>(2, false);
+
+        classifier.setDistanceCalculator(new EuclideanVectorCalculator<Double>());
+        classifier.setMajorityVoter(new ClassificationVoter());
+
+
+        classifier.train(dataSet, labels);
+
+        Vector r = new Vector(3.1, 2.2);
+        Integer classIdx = classifier.predict(r);
+
+        System.out.println("Point " + r + " has class index " + classIdx);
+
+        r = new Vector(9.1, 6.2);
+        classIdx = classifier.predict(r);
+    }
+}
---
+++ src/main/java/examples/ml/example4/Example4.java
@@ -0,0 +1,102 @@
+package examples.ml.example4;
+
+import maths.DenseMatrixSet;
+import maths.RowBuilder;
+import maths.Vector;
+import maths.RowType;
+import maths.functions.distances.EuclideanVectorCalculator;
+import ml.classifiers.ThreadedKNNClassifier;
+import parallel.partitioners.MatrixRowPartitionPolicy;
+import parallel.partitioners.RangePartitioner;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.columns.Column;
+import ml.classifiers.utils.ClassificationVoter;
+import utils.Pair;
+import utils.PairBuilder;
+import utils.TableDataSetLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
+
+/** Category: Machine Learning
+ * ID: Example8
+ * Description: Classification with vanilla ParallelKNN algorithm
+ * Taken From:
+ * Details:
+ * TODO
+ */
+public class Example4 {
+
+    public static Pair<DenseMatrixSet<Double>, List<Integer>> createDataSet() throws IOException, IllegalArgumentException {
+
+        // load the data
+        Table dataSetTable = TableDataSetLoader.loadDataSet(new File("src/main/resources/datasets/iris_data.csv"));
+
+        List<Integer> labels = new ArrayList<>();
+
+        Column species  = dataSetTable.column("species");
+
+        for (int i = 0; i < species.size(); i++) {
+
+            String label = (String) species.get(i);
+
+            if(label.equals("Iris-setosa")){
+
+                labels.add(0);
+            }
+            else if(label.equals("Iris-versicolor")){
+
+                labels.add(1);
+            }
+            else if(label.equals("Iris-virginica")){
+
+                labels.add(2);
+            }
+            else{
+                throw new IllegalArgumentException("Unknown class");
+            }
+        }
+
+        Table reducedDataSet = dataSetTable.removeColumns("species").first(dataSetTable.rowCount());
+        DenseMatrixSet<Double> dataSet = new DenseMatrixSet(RowType.Type.DOUBLE_VECTOR, new RowBuilder());
+        dataSet.initializeFrom(reducedDataSet);
+
+        // partition the data set
+        List<List<Integer>> partitions = RangePartitioner.partition(0, dataSet.m(), 4);
+
+        MatrixRowPartitionPolicy partitionPolicy = new MatrixRowPartitionPolicy(partitions);
+        dataSet.setPartitionPolicy(partitionPolicy);
+
+        return PairBuilder.makePair(dataSet, labels);
+    }
+
+    public static void main(String[] args) throws IOException, IllegalArgumentException{
+
+        Pair<DenseMatrixSet<Double>, List<Integer>> data = Example4.createDataSet();
+        ExecutorService executorService = newFixedThreadPool(4);
+
+        System.out.println("Number of rows: "+data.first.m());
+        System.out.println("Number of labels: "+data.second.size());
+
+
+        ThreadedKNNClassifier<Double, DenseMatrixSet<Double>, EuclideanVectorCalculator<Double>,
+                ClassificationVoter> classifier = new ThreadedKNNClassifier<>(3, false, executorService);
+
+        classifier.setDistanceCalculator(new EuclideanVectorCalculator<Double>());
+        classifier.setMajorityVoter(new ClassificationVoter());
+
+        classifier.train(data.first, data.second);
+        Vector point = new Vector(5.9,3.0,5.1,1.8);
+        Integer classIdx = classifier.predict(point);
+
+        System.out.println("Point "+ point +" has class index "+ classIdx);
+        executorService.shutdown();
+
+    }
+}
