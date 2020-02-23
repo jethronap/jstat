51d85950b2f83e64caf8e29c8d8511a61a286a4f
jethronap <efthioa@gmail.com>
2020-02-23 13:22:49 +0200

    added interface for centroid type

---
+++ src/main/java/ml/clustering/ICentroidInitializerType.java
@@ -0,0 +1,10 @@
+package ml.clustering;
+
+import java.util.List;
+
+public interface ICentroidInitializerType {
+
+     static <ClusterType, DataSetType> void initialize(List<ClusterType> cluster, DataSetType dataSet, int k) {
+
+    }
+}
