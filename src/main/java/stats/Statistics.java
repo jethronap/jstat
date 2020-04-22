f9929f112f5337eec418253e823eb1899ee6cdea
Alexandros Giavaras <a.giavaras@gmail.com>
2020-04-22 16:32:50 +0100

    #75 Add statistics calculation to Statistics class

--- src/main/java/stats/Statistics.java
+++ src/main/java/stats/Statistics.java
@@ -1,6 +1,11 @@
 package stats;
 
 
+import datasets.VectorDouble;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import utils.ListUtils;
+
+import java.util.List;
 
 /**
  * Simple class that holds some basic statistics usually
@@ -34,5 +39,55 @@ public class Statistics {
         str += "Kurtosis:  "+new Double(kurtosis).toString()+"\n";
         str += "Valid:     "+new Boolean(isValid).toString()+"\n";
 	    return str;
+    }
+
+    /**
+     *
+     * @param data The data to calculate the statistics
+     * @return Statistics instance
+     */
+    public static Statistics calculate(List<Double> data){
+
+	    if(data.isEmpty()){
+	        throw new IllegalArgumentException("The data set given is empty");
+        }
+
+	    Statistics statistics = new Statistics();
+        DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics(ListUtils.toDoubleArray(data));
+        statistics.mean = descriptiveStatistics.getMean();
+        statistics.median = descriptiveStatistics.getPercentile(50.0);
+        statistics.variance = descriptiveStatistics.getVariance();
+        statistics.max = descriptiveStatistics.getMax();
+        statistics.min = descriptiveStatistics.getMin();
+        statistics.kurtosis = descriptiveStatistics.getKurtosis();
+        statistics.skewness = descriptiveStatistics.getSkewness();
+        statistics.isValid = true;
+        return  statistics;
+
+    }
+
+    /**
+     *
+     * @param data The data to calculate the statistics
+     * @return Statistics instance
+     */
+    public static Statistics calculate(VectorDouble data){
+
+        if(data.empty()){
+            throw new IllegalArgumentException("The data set given is empty");
+        }
+
+        Statistics statistics = new Statistics();
+        DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics(ListUtils.toDoubleArray(data.toArray()));
+        statistics.mean = descriptiveStatistics.getMean();
+        statistics.median = descriptiveStatistics.getPercentile(50.0);
+        statistics.variance = descriptiveStatistics.getVariance();
+        statistics.max = descriptiveStatistics.getMax();
+        statistics.min = descriptiveStatistics.getMin();
+        statistics.kurtosis = descriptiveStatistics.getKurtosis();
+        statistics.skewness = descriptiveStatistics.getSkewness();
+        statistics.isValid = true;
+        return  statistics;
+
     }
 }
