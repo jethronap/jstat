61211d8f958cb4ce3c3513b4729c409605a11a5a
Alex <a.giavaras@gmail.com>
2021-02-27 18:24:00 +0000

    Change dataPath from File to String

--- src/main/java/jstat/base/Configuration.java
+++ src/main/java/jstat/base/Configuration.java
@@ -5,13 +5,11 @@ import java.io.File;
 public class Configuration {
 
     public static boolean ENABLE_WARNINGS = true;
-
-    public static File dataDirectory = new File("src/main/resources/jstat/datasets/car_plant.csv");
+    public static String dataDirectory = "src/main/resources/jstat/datasets/";
 
     public static class Logging
     {
         public static String WARN_MSG = "WARNING: ";
-
         public static void printWarning(String warningMsg){
             System.out.println(WARN_MSG + warningMsg);
         }
