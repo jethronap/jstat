02baff14b688719d417bcd4ab822ace6f538912e
Alex <a.giavaras@gmail.com>
2021-03-13 20:09:33 +0000

    Update API

--- src/main/java/jstat/dataloader/CSVDataLoader.java
+++ src/main/java/jstat/dataloader/CSVDataLoader.java
@@ -32,18 +32,31 @@ public class CSVDataLoader {
     }
 
     public static Pair<INDArray, INDArray> loadCarPlantWithIntercept() throws IOException {
+        return CSVDataLoader.loadCarPlantWithIntercept(0);
+    }
+
+    public static Pair<INDArray, INDArray> loadCarPlantWithIntercept(int extraCols) throws IOException {
 
         File file = new File(Configuration.dataDirectory + "car_plant.csv");
 
         CsvReadOptions options = CsvReadOptions.builder(file).missingValueIndicator("null").build();
         Table dataSet = Table.read().usingOptions(options);
 
-        INDArray x = Nd4j.zeros(dataSet.rowCount(), 2);
+        int cols = 2 + extraCols;
+
+        INDArray x = Nd4j.zeros(dataSet.rowCount(), cols);
         INDArray y = Nd4j.zeros(dataSet.rowCount());
 
         for(int i=0; i<dataSet.rowCount(); ++i){
             x.putScalar(i, 0, 1.0);
             x.putScalar(i, 1, dataSet.doubleColumn(0).get(i));
+
+            if(cols > 2){
+                for(int j=2; j<x.size(1); ++j){
+                    x.putScalar(i, j, dataSet.doubleColumn(0).get(i));
+                }
+            }
+
             y.putScalar(i, dataSet.doubleColumn(1).get(i));
         }
 
